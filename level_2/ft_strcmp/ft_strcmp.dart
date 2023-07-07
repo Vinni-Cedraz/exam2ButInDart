94420f3f45f948de8081632fbd940c9a9a6b88d2
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-07-07 20:50:43 +0000

    doing it with Class now

--- level_2/ft_strcmp/ft_strcmp.dart
+++ level_2/ft_strcmp/ft_strcmp.dart
@@ -8,35 +8,49 @@ int ft_strcmp(String s1, String s2) {
     }
     i++;
   }
-  if (s1.isEmpty && s2.isEmpty)
-    return (0);
-  else if (s1.isEmpty)
-    return (-1);
-  else if (s2.isEmpty) return (1);
-  if (s1.length > s2.length) {
-    if (s1.codeUnitAt(i) > s2.codeUnitAt(i - 1))
-      return (1);
-    else if (s1.codeUnitAt(i) < s2.codeUnitAt(i - 1)) {
-      return (-1);
-    }
-  } else if (s1.length < s2.length) {
-    if (s1.codeUnitAt(i - 1) > s2.codeUnitAt(i))
-      return (1);
-    else if (s1.codeUnitAt(i - 1) < s2.codeUnitAt(i)) {
-      return (-1);
+  edgeCaseHandler handler = edgeCaseHandler();
+  handler.caseOneIsEmpty(s1, s2);
+  handler.caseOneIsShorter(s1, s2, i);
+  return (handler.result);
+}
+
+// Create a class to orgazine and refactor the mess of the Edge cases above:
+class edgeCaseHandler {
+  int result = 0;
+  caseOneIsEmpty(String s1, String s2) {
+    if (s1.isEmpty && s2.isEmpty)
+      this.result = 0;
+    else if (s1.isEmpty)
+      this.result = -1;
+    else if (s2.isEmpty) this.result = 1;
+  }
+
+  caseOneIsShorter(String s1, String s2, int i) {
+    if (s1.isEmpty || s2.isEmpty) return;
+    if (s1.length > s2.length) {
+      if (s1.codeUnitAt(i) > s2.codeUnitAt(i - 1))
+        this.result = 1;
+      else if (s1.codeUnitAt(i) < s2.codeUnitAt(i - 1)) {
+        this.result = -1;
+      }
+    } else if (s1.length < s2.length) {
+      if (s1.codeUnitAt(i - 1) > s2.codeUnitAt(i))
+        this.result = 1;
+      else if (s1.codeUnitAt(i - 1) < s2.codeUnitAt(i)) {
+        this.result = -1;
+      }
     }
   }
-  return (0);
 }
 
 //tests:
 void main() {
   print(ft_strcmp("abc", "abc"));
   print(ft_strcmp("abc", "abd"));
-  print(ft_strcmp("abc", "zzz"));
-  print(ft_strcmp("abc", "ab"));
-  print(ft_strcmp("ab", "abc"));
   print(ft_strcmp("", "abc"));
   print(ft_strcmp("abc", ""));
   print(ft_strcmp("", ""));
+  print(ft_strcmp("abc", "zzz"));
+  print(ft_strcmp("abc", "ab"));
+  print(ft_strcmp("ab", "abc"));
 }
